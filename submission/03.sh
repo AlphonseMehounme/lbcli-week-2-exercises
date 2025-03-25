68a477775e2156bbaaf29eef706ac6f307c2fe2a
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-03-25 20:45:02 +0100

    fix: add fund to address

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,6 @@
 # Created a SegWit address.
-address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+address=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 10 $address 
+blocks=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" generatetoaddress 101 "$address")
 # Return only the Address
 echo $address
