25f7e53013de3c1e538a47d07b6c31cc5e2b3c44
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-03-25 11:49:25 +0100

    generate address, add fund and list UTXOs

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Created a SegWit address.
+address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # Add funds to the address.
+bitcoin-cli generatetoaddress 10 $address 
 # Return only the Address
+echo $address
