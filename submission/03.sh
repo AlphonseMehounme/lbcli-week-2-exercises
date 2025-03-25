cdb82efe9d7d0e9fb6874e909c4752ede6a3bed8
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-03-25 14:08:43 +0100

    total output value

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,6 @@
 # Created a SegWit address.
 address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # Add funds to the address.
-bitcoin-cli generatetoaddress 10 $address 
+bitcoin-cli -regtest generatetoaddress 10 $address 
 # Return only the Address
 echo $address
